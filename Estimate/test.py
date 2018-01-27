47541829ce73e6a1cde5adc85cc943dad4563c6e
fffasttime <1148716581@qq.com>
2018-01-27 23:54:05 +0800

    Remove useless files

--- Estimate/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-print("[Info] Loading nn module")
-from nn import TFProcess
-network=TFProcess(False, True)
-print("[Info] nn module loaded")
-
-raw_input=np.zeros([1,2,225])
-print(network.forward(raw_input))
