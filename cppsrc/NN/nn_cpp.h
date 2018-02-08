ac93a8f29015a7dad22f668e4cff77590dd75d23
syys96 <3359652182@qq.com>
2018-02-08 19:19:33 +0800

    Update nn_cpp.h

--- cppsrc/NN/nn_cpp.h
+++ cppsrc/NN/nn_cpp.h
@@ -35,6 +35,8 @@ public:
 	void init_global_objects();
 	void set_maxplayout(int maxplayout);
 	Network::NN_Ouputs forward(Network::NNPlanes input_planes);
+private:
+	std::string cfg_weightsfile;
 };
 
 
